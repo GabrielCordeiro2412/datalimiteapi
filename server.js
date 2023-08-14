4989d1cb82a84a773fb946cbf5cf51646c4e3628
GabrielCordeiro2412 <bielcordsgames.net@gmail.com>
2023-08-14 17:30:36 -0300

    Correção porta

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.delete('/messages/:index', async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
