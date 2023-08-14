354f69b553e7a28788e8aebf63cdfd9d31a3a1e0
GabrielCordeiro2412 <bielcordsgames.net@gmail.com>
2023-08-14 18:04:16 -0300

    porta

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.delete('/messages/:index', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
