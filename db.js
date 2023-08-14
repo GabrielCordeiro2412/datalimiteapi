0b335c02d7e153506f84a6ce52aaf3f0f008e6a1
GabrielCordeiro2412 <bielcordsgames.net@gmail.com>
2023-08-14 19:27:32 -0300

    c

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gcordeiro609:tz4Rywv7yLktXPco@datalimite.saosgde.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://gcordeiro609:lpu2XVKVRqxqefMz@datalimite.bjfhou9.mongodb.net/?retryWrites=true&w=majority')
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
